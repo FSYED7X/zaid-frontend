037a3ed2e5299c31102df42caec105e22c52c6b7
fsyed7x <fsyed7x@gmail.com>
2023-11-29 07:43:36 +0530

    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+
+axios.defaults.baseURL = process.env.REACT_APP_API_ENDPOINT;
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
